e03f1e55d634a11c71121796b452fe401b672988
Robert Hamilton <robert.hamilton@shell.com>
2024-09-26 11:53:16 -0500

    feat: detect shared dependency conflicts

--- packages/config/__tests__/validators.spec.ts
+++ packages/config/__tests__/validators.spec.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import { cloneDeep, set } from 'lodash';
 import { AppshellManifestValidator, AppshellTemplateValidator } from '../src/validators';
 import appshellConfigRemoteCollisions from './assets/appshell.config-remote-collision.json';
 import validManifest from './assets/appshell.json';
@@ -62,6 +63,178 @@ describe('validators', () => {
       );
 
       expect(consoleSpy).toHaveBeenCalledWith(chalk.yellow('Multiple remotes with the same key'));
+    });
+
+    describe('hasSharedDependencyConflict', () => {
+      it('should not warn if does not have shared dependency conflict', () => {
+        const incomingManifest = cloneDeep(validManifest);
+
+        set(incomingManifest, 'remotes', {});
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package1: 0.1.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package2: 1.1.0',
+          ),
+        );
+      });
+
+      it('should log if incoming manifest has new dependency', () => {
+        const incomingManifest = cloneDeep(validManifest);
+
+        set(incomingManifest, 'remotes', {});
+        set(incomingManifest, 'modules.TestModule.shared.package3', {
+          singleton: true,
+          requiredVersion: '1.0.0',
+        });
+
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package1: 0.1.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package2: 1.1.0',
+          ),
+        );
+      });
+
+      it('should log matches if incoming manifest has shared dependency that is in range', () => {
+        const incomingManifest = cloneDeep(validManifest);
+
+        set(incomingManifest, 'remotes', {});
+        set(validManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: '^1.0.0',
+        });
+        set(incomingManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: '1.1.0',
+        });
+
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package1: 0.1.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package package2: ^1.0.0',
+          ),
+        );
+      });
+
+      it('should log conflict if incoming manifest has shared dependency that is out of range', () => {
+        const incomingManifest = cloneDeep(validManifest);
+
+        set(incomingManifest, 'remotes', {});
+        set(validManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: '^1.0.0',
+        });
+        set(incomingManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: '2.0.0',
+        });
+
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.magenta(
+            'Shared dependency version conflict in MFE module TestModule for package package2: 2.0.0 does not satisfy ^1.0.0',
+          ),
+        );
+      });
+
+      it('should log missing version if incoming manifest has shared dependency that is undefined', () => {
+        const incomingManifest = cloneDeep(validManifest);
+
+        set(incomingManifest, 'remotes', {});
+        set(validManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: '^1.0.0',
+        });
+        set(incomingManifest, 'modules.TestModule.shared.package2', {
+          singleton: true,
+          requiredVersion: undefined,
+        });
+
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.magenta(
+            'Shared dependency version missing in MFE module TestModule for package package2: undefined',
+          ),
+        );
+      });
+
+      it('should log matches when shared dependencies are identical', () => {
+        const incomingManifest = cloneDeep(validManifest);
+        const shared = {
+          react: {
+            singleton: true,
+            requiredVersion: '18.2.0',
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: '18.2.0',
+          },
+          'react-refresh': {
+            singleton: true,
+            requiredVersion: '^0.14.0',
+          },
+          'styled-components': {
+            singleton: true,
+            requiredVersion: '6.0.0-rc.3',
+          },
+          '@appshell/react': {
+            singleton: true,
+            requiredVersion: '^0.3.0',
+          },
+        };
+
+        set(incomingManifest, 'remotes', {});
+        set(validManifest, 'modules.TestModule.shared', shared);
+        set(incomingManifest, 'modules.TestModule.shared', shared);
+
+        AppshellManifestValidator.validate<AnyConfig>(validManifest, incomingManifest);
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package react: 18.2.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package react-dom: 18.2.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package react-refresh: ^0.14.0',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package styled-components: 6.0.0-rc.3',
+          ),
+        );
+        expect(consoleSpy).toHaveBeenCalledWith(
+          chalk.green(
+            'Shared dependency version matched in MFE module TestModule for package @appshell/react: ^0.3.0',
+          ),
+        );
+      });
     });
   });
 });
